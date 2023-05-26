99fb0a1dcf914e56d1b492b8dbfb905a6ae47925
maximecornaton <maxime.cornaton@cpe.fr>
2023-05-26 15:54:11 +0200

    proxy

--- projet_majeur/src/main/resources/static/js/fragments-manager.js
+++ projet_majeur/src/main/resources/static/js/fragments-manager.js
@@ -37,12 +37,7 @@ function processMapFragment(fragmentElement, fragmentContent) {
     const mapDiv = tempDiv.querySelector('#map');
 
     // Créer l'élément iframe
-    const iframe = document.createElement('iframe');
-    iframe.src = "https://api.mapbox.com/styles/v1/maximecrnt/cli36w9f700ko01pgahnu8l6o.html?title=false&access_token=pk.eyJ1IjoibWF4aW1lY3JudCIsImEiOiJjbGkzNmdzbW4wcTJtM2ZvM2Z4azYxbTcwIn0.AmmVszgHGmq9KMbzdmhe7A&zoomwheel=false#11.93/45.76267/4.84871";
-    iframe.title = "Monochrome";
-    iframe.style.border = "none";
-    iframe.style.width = "100%";
-    iframe.style.height = "100%";
+    iframe = createFrame();
 
     // Vider le contenu de l'élément fragment
     fragmentElement.innerHTML = "";
