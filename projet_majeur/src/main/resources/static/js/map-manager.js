4a52a598077a6c0d6d5444400240702e9df70be2
maximecornaton <maxime.cornaton@cpe.fr>
2023-05-26 17:56:27 +0200

    proxy

--- projet_majeur/src/main/resources/static/js/map-manager.js
+++ projet_majeur/src/main/resources/static/js/map-manager.js
@@ -1,36 +1,31 @@
-function createFrame(){
-    // Créer l'élément iframe
-    const iframe = document.createElement('iframe');
-    iframe.src = "https://api.mapbox.com/styles/v1/maximecrnt/cli36w9f700ko01pgahnu8l6o.html?title=false&access_token=pk.eyJ1IjoibWF4aW1lY3JudCIsImEiOiJjbGkzNmdzbW4wcTJtM2ZvM2Z4azYxbTcwIn0.AmmVszgHGmq9KMbzdmhe7A&zoomwheel=false#11.93/45.76267/4.84871";
-    iframe.title = "Monochrome";
-    iframe.style.border = "none";
-    iframe.style.width = "100%";
-    iframe.style.height = "100%";
 
-    displayFires(iframe, getFires()); // Appel de la fonction displayFires avec les données des feux
+function createMap(divId) {
+    console.log(divId);
+    let map = L.map(divId).setView([45.7735, 4.8745], 13);
 
-    return iframe;
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox/streets-v11',
+        tileSize: 512,
+        zoomOffset: -1,
+
+        accessToken: 'pk.eyJ1IjoibWF4aW1lY3JudCIsImEiOiJjbGkzNmdzbW4wcTJtM2ZvM2Z4azYxbTcwIn0.AmmVszgHGmq9KMbzdmhe7A'
+    }).addTo(map);
+
+    return map;
 }
 
-function displayFires(iframe, firesPromise) {
-    firesPromise
-        .then(firesData => {
-            console.log(firesData);
-
-            firesData.forEach(fire => {
-                const { latitude, longitude } = fire;
-                console.log(latitude, longitude);
-                // Créer un marqueur de feu
-                const marker = new iframe.contentWindow.mapboxgl.Marker({ color: '#FF0000' })
-                    .setLngLat([longitude, latitude])
-                    .addTo(iframe.contentWindow.map); // Ajouter le marqueur à la carte de l'iframe
-            });
-        })
-        .catch(error => {
-            console.error('Une erreur s\'est produite:', error);
+function displayFires(map) {
+getFires().then(fires => {
+        fires.forEach(fire => {
+            L.marker([fire.lat, fire.lon]).addTo(map)
+                .bindPopup('Feu')
+                .openPopup();
         });
-}
+    });
 
+}
 
 
 function getFires() {
@@ -44,11 +39,4 @@ function getFires() {
         .catch(error => {
             console.error('Une erreur s\'est produite:', error);
         });
-}
-
-
-
-
-function getCaserne(){
-
 }
