434c1cbb52a27d5970f3acfba9eda4ae73b43549
maximecornaton <maxime.cornaton@cpe.fr>
2023-06-01 00:02:13 +0200

    oui

--- projet_majeur/src/main/resources/static/js/map-manager.js
+++ projet_majeur/src/main/resources/static/js/map-manager.js
@@ -16,14 +16,33 @@ function displayFires(map) {
 getFires().then(fires => {
         fires.forEach(fire => {
             const icon = L.icon({
-                iconUrl: '../img/icons/fire.gif', // Remplacez par l'URL de votre image de camion de pompier
-                iconSize: [32, 32], // Taille de l'icône en pixels
-                iconAnchor: [16, 16], // Point d'ancrage de l'icône en pixels
+                iconUrl: '../img/icons/fire.gif',
+                iconSize: [32, 32],
+                iconAnchor: [16, 16],
             });
 
-            L.marker([fire.lat, fire.lon], { icon: icon }).addTo(map)
-                .bindPopup('Feu')
-                .openPopup();
+            L.marker([fire.lat, fire.lon], { icon: icon }).addTo(map);
+
+            const popupContent = `
+                <strong>ID:</strong> ${fire.id}<br>
+                
+            `;
+
+            marker.bindPopup(popupContent);
+
+            marker.on('click', function() {
+                const newIcon = L.icon({
+                    iconUrl: '../img/icons/fire.gif',
+                    iconSize: [40, 40],
+                    iconAnchor: [20, 20],
+                });
+                marker.setIcon(newIcon);
+            });
+
+            marker.on('popupclose', function() {
+                marker.setIcon(icon);
+            });
+
         });
     });
 }
@@ -32,18 +51,40 @@ function displayVehicles(map) {
     getVehicles().then(vehicles => {
         vehicles.forEach(vehicle => {
             const icon = L.icon({
-                iconUrl: '../img/icons/fire-truck.png', // Remplacez par l'URL de votre image de camion de pompier
-                iconSize: [32, 32], // Taille de l'icône en pixels
-                iconAnchor: [16, 16], // Point d'ancrage de l'icône en pixels
+                iconUrl: '../img/icons/fire-truck.png',
+                iconSize: [32, 32],
+                iconAnchor: [16, 16],
             });
 
-            L.marker([vehicle.lat, vehicle.lon], { icon: icon }).addTo(map)
-                .bindPopup('Camion')
-                .openPopup();
+            const marker = L.marker([vehicle.lat, vehicle.lon], { icon: icon }).addTo(map);
+
+            const popupContent = `
+                <strong>ID:</strong> ${vehicle.id}<br>
+                <strong>Type:</strong> ${vehicle.type}<br>
+                <strong>Type de Liquide:</strong> ${vehicle.liquidType}<br>
+                <strong>Quantit&#xE9 de Liquide:</strong> ${vehicle.liquidQuantity}<br>
+                <strong>Essence:</strong> ${vehicle.fuel}<br>
+            `;
+
+            marker.bindPopup(popupContent);
+
+            marker.on('popupopen', function() {
+                // Changez l'icône du camion lorsque le marqueur est cliqué
+                const newIcon = L.icon({
+                    iconUrl: '../img/icons/fire-truck.gif',
+                    iconSize: [40, 40],
+                    iconAnchor: [20, 20],
+                });
+                marker.setIcon(newIcon);
+            });
+
+            marker.on('popupclose', function() {
+                marker.setIcon(icon);
+            });
+
         });
     });
 }
-
 
 map = createMap('map');
 displayFires(map);
