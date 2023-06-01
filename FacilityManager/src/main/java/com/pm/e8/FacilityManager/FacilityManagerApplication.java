dccf0ec903b75b5918f82d37cb04ee3d87e40d8d
valbrqd <valbrqd@gmail.com>
2023-06-01 17:01:05 +0200

    autres

---
+++ FacilityManager/src/main/java/com/pm/e8/FacilityManager/FacilityManagerApplication.java
@@ -0,0 +1,13 @@
+package com.pm.e8.FacilityManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacilityManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacilityManagerApplication.class, args);
+	}
+
+}
