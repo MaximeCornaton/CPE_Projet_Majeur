3a6c3bfd99fdded284c782a9dca44c8aac6c9876
valbrqd <valbrqd@gmail.com>
2023-06-07 14:04:03 +0200

    interventions

--- Intervention/src/main/java/com/pm/e8/Intervention/service/InterventionService.java
+++ Intervention/src/main/java/com/pm/e8/Intervention/service/InterventionService.java
@@ -1,6 +1,7 @@
 package com.pm.e8.Intervention.service;
 
 import com.google.common.collect.Lists;
+import com.pm.e8.Intervention.model.Coordonnees;
 import com.pm.e8.Intervention.model.Intervention;
 import com.pm.e8.Intervention.model.Status;
 import com.pm.e8.Intervention.repository.InterventionRepository;
@@ -9,8 +10,10 @@ import com.project.model.dto.Coord;
 import com.project.model.dto.FireDto;
 import com.project.model.dto.LiquidType;
 import com.project.model.dto.VehicleDto;
+import org.checkerframework.checker.nullness.qual.AssertNonNullIfNonNull;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -28,8 +31,25 @@ public class InterventionService {
         this.mapRestClientService = mapRestClientService;
     }
 
+    public void AutoIntervention(int fireId, int vehicleId, List<Coordonnees> coordList){
+        FireDto fire = fireRestClientService.getFire(fireId);
+        LiquidType liquidType = getMostEfficientLiquid(fire.getType());
+
+        Intervention I = new Intervention(fireId, vehicleId);
+        for(Intervention i : iRepo.findAll()){
+            if((i.getIdFire() == fireId || I.getIdFire() == -1)){
+                return;
+            }
+        }
+        I.setCoordonnees(coordList);
+
+        iRepo.save(I);
+
+        vehicleRestClientService.updateVehicleLiquidType(vehicleId, liquidType);
+    }
+
     public void createIntervention(int fireId, int vehicleId) {
-        //TODO Verfiier que on peut automatiser le mouvement en creant des interventions
+
         FireDto fire = fireRestClientService.getFire(fireId);
         LiquidType liquidType = getMostEfficientLiquid(fire.getType());
         VehicleDto vehicle = vehicleRestClientService.getVehicle(vehicleId);
@@ -37,7 +57,10 @@ public class InterventionService {
         Coord coord = new Coord(fire.getLon(),fire.getLat());
         String polyline = mapRestClientService.getPolyline(new Coord(vehicle.getLon(),vehicle.getLat()),coord);
         List<Coord> coordList = PolylineSplitter.cutPolyline(polyline, vehicle.getType().getMaxSpeed()/1000);
-
+        List<Coordonnees> coordonneesList = new ArrayList<>();
+        for(Coord c : coordList){
+            coordonneesList.add(new Coordonnees(c.getLon(),c.getLat()));
+        }
         for(Intervention i : iRepo.findAll()){
             if(i.getIdFire() == fireId){
                 return;
@@ -45,7 +68,7 @@ public class InterventionService {
         }
 
         Intervention I = new Intervention(fireId, vehicleId);
-        I.setCoordonnees(coordList);
+        I.setCoordonnees(coordonneesList);
         iRepo.save(I);
 
         vehicleRestClientService.updateVehicleLiquidType(vehicleId, liquidType);
