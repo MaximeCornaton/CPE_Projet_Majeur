86dec04974401fb5e03bd748e61d8073ff0aa7e7
Rodolphe Lajugie <rodolphe.lajugie@cpe.fr>
2023-06-07 14:34:33 +0200

    non electric

--- FleetM/src/main/java/com/pm/e8/FleetManager/Service/VehicleService.java
+++ FleetM/src/main/java/com/pm/e8/FleetManager/Service/VehicleService.java
@@ -201,8 +201,12 @@ public class VehicleService {
         while (!found){
             FireDto fireDto = fireDtoList.get(id);
             if (fireAvailable.isEmpty()){
-                fireAvailable.add(fireDto.getId());
-                found = true;
+                if (fireRestClientService.getFireDtoById(id).equals("E_Electric")){
+                    id++;
+                }else {
+                    fireAvailable.add(fireDto.getId());
+                    found = true;
+                }
             }else if (!fireAvailable.contains(fireDto.getId())){
                 fireAvailable.add(fireDto.getId());
                 found = true;
@@ -225,8 +229,10 @@ public class VehicleService {
     }
 
     public void clearFireAvailable(){
-        for (int i=0; i<fireAvailable.size(); i++){
-            if (fireRestClientService.getFireDtoById(fireAvailable.get(i)) == null){
+        for (int i=0; i<fireAvailable.size(); i++) {
+            if (fireRestClientService.getFireDtoById(fireAvailable.get(i)) == null) {
+                fireAvailable.remove(i);
+            } else if (fireRestClientService.getFireDtoById(fireAvailable.get(i)).getType().equals("E_ELECTRIC")) {
                 fireAvailable.remove(i);
             }
         }
