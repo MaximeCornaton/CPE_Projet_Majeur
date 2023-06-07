3882b087badab17706bf8d46d86f20b50fd58c82
valbrqd <valbrqd@gmail.com>
2023-06-07 15:41:02 +0200

    ok

--- FireM/src/main/java/com/pm/e8/FireManager/service/FireService.java
+++ FireM/src/main/java/com/pm/e8/FireManager/service/FireService.java
@@ -1,10 +1,12 @@
 package com.pm.e8.FireManager.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.collect.Lists;
 import com.pm.e8.FireManager.model.Fire;
 import com.pm.e8.FireManager.repository.FireRepository;
 import com.project.model.dto.*;
-import org.ejml.data.FMatrixSparseTriplet;
+
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -17,18 +19,19 @@ public class FireService {
     private final FireRepository fRepo;
     private VehicleRestService vehicleRestService;
 
-    public FireService(FireRestClientService fireRestClientService,FireRepository fRepo) {
+    public FireService(FireRestClientService fireRestClientService, FireRepository fRepo) {
         this.fireRestClientService = fireRestClientService;
         this.fRepo = fRepo;
     }
+
     public List<FireDto> getFireDtoList() {
         return fireRestClientService.getAllFires();
     }
 
 
-    public String getFireType(int idf){
+    public String getFireType(int idf) {
         Optional<Fire> f = fRepo.findById(idf);
-        if(f.isEmpty()){
+        if (f.isEmpty()) {
             return "No fire found";
         }
         return f.get().getType();
@@ -58,25 +61,27 @@ public class FireService {
     public List<Fire> getFireList() {
         return Lists.newArrayList(fRepo.findAll());
     }
+
     List<Fire> FiresAround = new ArrayList<Fire>();
+
     public List<Fire> getFiresAround(int idv, int radius) {
         List<Fire> fList = getFireList();
 
         VehicleDto v = vehicleRestService.getVehicle(idv);
 
-        for(Fire f : fList){
-                if(distance(v,f) <= radius){
-                    FiresAround.add(f);
+        for (Fire f : fList) {
+            if (distance(v, f) <= radius) {
+                FiresAround.add(f);
             }
         }
         return FiresAround;
     }
 
-    public double distance(VehicleDto v1, Fire f2){
-        return getDistanceBetweenCoords(new Coord(v1.getLat(),v1.getLon()),new Coord(f2.getLat(),f2.getLon()));
+    public double distance(VehicleDto v1, Fire f2) {
+        return getDistanceBetweenCoords(new Coord(v1.getLat(), v1.getLon()), new Coord(f2.getLat(), f2.getLon()));
     }
 
-    public Double getDistanceBetweenCoords(Coord coord1,Coord coord2){
+    public Double getDistanceBetweenCoords(Coord coord1, Coord coord2) {
         RestTemplate restTemplate = new RestTemplate();
         String url = "http://vps.cpe-sn.fr:8081/fire/distance";
         UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(url)
@@ -90,16 +95,16 @@ public class FireService {
     public List<FireDto> getTypeFires(String type) {
         List<Fire> fList = getFireList();
         List<FireDto> typeFires = new ArrayList<>();
-        for(Fire f : fList){
+        for (Fire f : fList) {
             FireDto fireDto = FireRestClientService.getFireDto(f.getId());
-            if(f.getType().equals(type)){
+            if (f.getType().equals(type)) {
                 typeFires.add(fireDto);
             }
         }
         return typeFires;
     }
 
-    public List<String> getTypes(){
+    public String getTypes() {
         List<String> typeList = new ArrayList<>();
         typeList.add("E_Electric");
         typeList.add("B_Gasoline");
@@ -108,8 +113,15 @@ public class FireService {
         typeList.add("B_Plastics");
         typeList.add("B_Alcohol");
         typeList.add("A");
-        return typeList;
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            String json = objectMapper.writeValueAsString(typeList);
+            return json;
+
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
-
-
 }
